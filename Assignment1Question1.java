309a331c055d47c95bbfaeab33bbcaacf1a5699f
jayanth-thota <60084399+jayanth-thota@users.noreply.github.com>
2020-01-28 23:30:26 +0530

    Add files via upload

---
+++ Assignment1Question1.java
@@ -0,0 +1,19 @@
+package assignment1;
+import java.util.Scanner;
+
+public class Assignment1Question1 {
+	
+		
+		public static void main(String[] args) {
+
+			 String name;
+			 int year;
+			 Scanner scanner= new Scanner(System.in);
+			 name= scanner.next();
+			 year =scanner.nextInt();
+			
+			 System.out.println("My name is "+name+" and I'll graduate in "+year);
+		}
+
+
+}
