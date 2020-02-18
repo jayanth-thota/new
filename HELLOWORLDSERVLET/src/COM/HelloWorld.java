45d9eb56b588d652a5631ac7b0418c0676c80b3e
jayanth-thota <60084399+jayanth-thota@users.noreply.github.com>
2020-02-18 14:27:00 +0530

    Add files via upload

---
+++ HELLOWORLDSERVLET/src/COM/HelloWorld.java
@@ -0,0 +1,43 @@
+package COM;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorld() {
+        // TODO Auto-generated constructor stub
+    	System.out.println("HelloWorld();");
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init();");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("service()");
+	}
+
+
+
+
+	}
