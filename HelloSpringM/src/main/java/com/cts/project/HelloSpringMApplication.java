c7dcf10f85c880eb8f5d51116617af1752038ecf
jayanth-thota <60084399+jayanth-thota@users.noreply.github.com>
2020-02-21 11:05:01 +0530

    Add files via upload

---
+++ HelloSpringM/src/main/java/com/cts/project/HelloSpringMApplication.java
@@ -0,0 +1,13 @@
+package com.cts.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringMApplication.class, args);
+	}
+
+}
